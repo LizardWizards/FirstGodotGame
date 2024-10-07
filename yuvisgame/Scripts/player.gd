a17cea14f3f240217b5fdbc191017f2b4b562a32
yuvikadube <yuvika.dube@misfitsgaming.gg>
2024-10-07 14:21:01 -0700

    added player

---
+++ yuvisgame/Scripts/player.gd
@@ -0,0 +1,25 @@
+extends Area2D
+@export var speed = 150
+@onready var screenSize = get_viewport_rect().size
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var input = Input.get_vector("left", "right", "up", "down")
+	position += input * speed * delta
+	position = position.clamp(Vector2(8,8), screenSize - Vector2(8,8))
+	print(input.x)
+	
+	if input.x > 0:
+		$Ship.frame = 2
+		$Ship/Boosters.animation = "right"
+	elif input.x < 0:
+		$Ship.frame = 0
+		$Ship/Boosters.animation = "left"
+	else:
+		$Ship.frame = 1
+		$Ship/Boosters.animation = "forward"
